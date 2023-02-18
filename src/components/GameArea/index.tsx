134c6b9d72787254feb3ed643063ac1705e0c31a
Gaurav Rana <smartgauravrana@gmail.com>
2023-02-18 12:02:22 +0530

    Virtual keyboard removed

--- src/components/GameArea/index.tsx
+++ src/components/GameArea/index.tsx
@@ -10,17 +10,17 @@ const WORDS = ["Knob", "Mat", "Bell"];
 
 function GameArea({}: Props) {
   const { setResultScreen } = useScreenStore((state) => state);
-  const [value, setValue] = useState("");
+  // const [value, setValue] = useState("");
   const [play] = useSound("/fanfare.mp3");
 
-  const handleKeyDown = (e: string) => {
-    if (e === "{enter}") {
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
       setResultScreen();
       play();
     }
   };
 
-  const handleChange = (val: any) => setValue(val);
+  // const handleChange = (val: any) => setValue(val);
 
   return (
     <div className="px-4">
@@ -38,13 +38,13 @@ function GameArea({}: Props) {
           name="answer"
           placeholder="Type from letters below"
           className=" text-lg leading-[21px] w-full bg-[#0C0C0C] border-[0.5px] border-solid border-[#DDDDDD] py-[14px] px-10 outline-none text-center"
-          // onKeyDown={handleKeyDown}
+          onKeyDown={handleKeyDown}
           autoComplete="off"
-          value={value}
-          readOnly
+          // value={value}
+          // readOnly
         />
       </div>
-      <CustomKeyboard onChange={handleChange} onKeyReleased={handleKeyDown} />
+      {/* <CustomKeyboard onChange={handleChange} onKeyReleased={handleKeyDown} /> */}
 
       <p className="text-[#828282] text-[15px] text-center leading-[18px] mt-[29px] ">
         Need Hint?
